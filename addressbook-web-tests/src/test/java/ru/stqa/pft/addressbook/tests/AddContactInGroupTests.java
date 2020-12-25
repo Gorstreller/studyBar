fa960e860eb9214c461ed0b3301db65935e90474
Kirill Vlasovets <vlasovetskm@gmail.com>
2020-12-25 17:12:58 +0300

    Промежуточный
    Просто создан новый тест

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactInGroupTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class AddContactInGroupTests extends TestBase{
+
+    @Test
+    public void testAddContactInGroup() {
+
+    }
+}
